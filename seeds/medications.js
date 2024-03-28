0c6294a3e09c86b79f1db765a631accb294b7362
Melanie <melanie.rawluk@gmail.com>
2024-03-28 15:05:19 -0600

    Update notes and medications seed files

--- seeds/medications.js
+++ seeds/medications.js
@@ -6,9 +6,11 @@ exports.seed = async function (knex) {
   // Deletes ALL existing entries
   await knex('medications').del()
   await knex('medications').insert([
-    { id: 1, active: true, name: 'Prozac', dose: '20mg', frequency: 'Twice daily', times: JSON.stringify(['9:00 AM', '6:00 PM']), start_date: new Date(), user_id: 1 },
-    { id: 2, active: true, name: 'Zoloft', dose: '50mg', frequency: 'Once daily', times: JSON.stringify(['8:00 AM']), start_date: new Date(), user_id: 1 },
-    { id: 3, active: false, name: 'Lexapro', dose: '10mg', frequency: 'Once daily', times: JSON.stringify(['10:00 AM']), start_date: new Date(), user_id: 1 },
-    { id: 4, active: false, name: 'Paxil', dose: '40mg', frequency: 'Once daily', times: JSON.stringify(['11:00 AM']), start_date: new Date(), user_id: 1 }
+    { id: 1, active: false, name: 'Prozac', dose: '20', frequency: 'Twice Daily', times: JSON.stringify(['9:00 AM', '6:00 PM']), start_date: '2024-03-01', end_date: '2024-03-18', user_id: 1 },
+    { id: 2, active: false, name: 'Zoloft', dose: '50', frequency: 'Once Daily', times: JSON.stringify(['8:00 AM']), start_date: '2024-03-01', end_date: '2024-03-18', user_id: 1 },
+    { id: 3, active: false, name: 'Lexapro', dose: '10', frequency: 'Once Daily', times: JSON.stringify(['10:00 AM']), start_date: '2024-03-01', end_date: '2024-03-18', user_id: 1 },
+    { id: 4, active: true, name: 'Wellbutrin', dose: '150', frequency: 'Once Daily', times: JSON.stringify(['9:00 AM']), start_date: '2024-03-20', user_id: 1 },
+    { id: 5, active: true, name: 'Lamotrigine', dose: '200', frequency: 'Twice Daily', times: JSON.stringify(['9:00 AM', '6:00 PM']), start_date: '2024-03-20', user_id: 1 },
+    { id: 6, active: true, name: 'Buspirone', dose: '25', frequency: 'Twice Daily', times: JSON.stringify(['9:00 AM', '6:00 PM']), start_date: '2024-03-20', user_id: 1 }
   ]);
 };
--- seeds/notes.js
+++ seeds/notes.js
@@ -8,19 +8,19 @@ exports.seed = async function (knex) {
   await knex('notes').insert([
     {
       id: 1,
-      date: '2023-11-11',
+      date: '2024-03-11',
       note_content: 'Feeling optimistic about the future.',
       user_id: 1
     },
     {
       id: 2,
-      date: '2023-11-15',
+      date: '2024-03-15',
       note_content: 'Experienced mild nausea and headache this morning.',
       user_id: 1
     },
     {
       id: 3,
-      date: '2023-12-01',
+      date: '2024-03-18',
       note_content: 'Feeling more energetic and focused.',
       user_id: 1
     },
