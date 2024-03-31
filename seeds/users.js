0878a1d74071b1341164a9785bf7a046e18b16ac
Melanie <melanie.rawluk@gmail.com>
2024-03-31 13:43:03 -0600

    Add hashed password to user seeds file

--- seeds/users.js
+++ seeds/users.js
@@ -6,6 +6,6 @@ exports.seed = async function (knex) {
   // Deletes ALL existing entries
   await knex('users').del()
   await knex('users').insert([
-    { id: 1, name: 'Leeland Eyelet', email: 'leeland@goldendoodle.com', password: '1234' },
+    { id: 1, name: 'Leeland Eyelet', email: 'leeland@goldendoodle.com', password: '$2a$10$h/PovQRdQBIGoH3iQOk1wubAL/wyNRcJ4HbQHYzPVKTWIN4SPvzQS' },
   ]);
 };
